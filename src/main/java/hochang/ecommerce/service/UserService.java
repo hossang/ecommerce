eac91b5026b92440c0a56ac37fe716d0263bbb58
hossang <60059710+hossang@users.noreply.github.com>
2023-04-15 22:03:16 +0900

    feat(UserService) : Add validateDuplicateUser() to validate duplicate User

--- src/main/java/hochang/ecommerce/service/UserService.java
+++ src/main/java/hochang/ecommerce/service/UserService.java
@@ -16,6 +16,13 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
+    private void validateDuplicateUser(User user) {
+        User findUser = userRepository.findByOnlineId(user.getOnlineId());
+        if (findUser != null) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
     private User toUser(UserForm userForm) {
         User user = User.builder().onlineId(userForm.getOnlineId())
                 .password(userForm.getPassword())
