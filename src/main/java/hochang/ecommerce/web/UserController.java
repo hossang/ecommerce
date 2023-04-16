9115e22d9deada2b6dad016f513725980d724620
hossang <60059710+hossang@users.noreply.github.com>
2023-04-16 15:11:10 +0900

    feat(UserController) : Add createForm() GET requesting a user registration form to /signup

---
+++ src/main/java/hochang/ecommerce/web/UserController.java
@@ -0,0 +1,23 @@
+package hochang.ecommerce.web;
+
+import hochang.ecommerce.dto.UserForm;
+import hochang.ecommerce.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/signup")
+    public String createForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "users/signup";
+    }
+}
