57565d1a7e6262d4dd5924068fe104222f97c518
hossang <60059710+hossang@users.noreply.github.com>
2023-04-28 21:22:39 +0900

    feat(WebConfig) :Register SignInUserArgumentResolver in WebConfig

---
+++ src/main/java/hochang/ecommerce/config/WebConfig.java
@@ -0,0 +1,15 @@
+package hochang.ecommerce.config;
+
+import hochang.ecommerce.web.argumentresolver.SignInUserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SignInUserArgumentResolver());
+    }
+}
