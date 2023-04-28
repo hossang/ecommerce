5b572f1dd50dd085b3e4eb098d26ca433cbf0d3c
hossang <60059710+hossang@users.noreply.github.com>
2023-04-28 22:10:27 +0900

    feat(WebConfig) : Register interceptor to check authentication

--- src/main/java/hochang/ecommerce/config/WebConfig.java
+++ src/main/java/hochang/ecommerce/config/WebConfig.java
@@ -1,13 +1,22 @@
 package hochang.ecommerce.config;
 
 import hochang.ecommerce.web.argumentresolver.SignInUserArgumentResolver;
+import hochang.ecommerce.web.interceptor.SignInCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SignInCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/users/**", "/admin/**");
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SignInUserArgumentResolver());
