cc9edf4513785374013c3bcd631c1839eb6de41b
hossang <60059710+hossang@users.noreply.github.com>
2023-04-29 11:01:49 +0900

    feat(UserController) : Add signIn() POST requesting attempting to /Sign-in

--- src/main/java/hochang/ecommerce/web/UserController.java
+++ src/main/java/hochang/ecommerce/web/UserController.java
@@ -4,13 +4,18 @@ import hochang.ecommerce.dto.SignInForm;
 import hochang.ecommerce.dto.UserForm;
 import hochang.ecommerce.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UserController {
@@ -34,5 +39,22 @@ public class UserController {
     @GetMapping("/sign-in")
     public String createSignInForm(SignInForm signInForm) {
         return "guests/signIn";
+    }
+
+    @PostMapping("/sign-in")
+    public String signIn(@Valid SignInForm signInForm, BindingResult bindingResult
+            , HttpServletRequest request, @RequestParam(defaultValue = "/") String redirectURL) {
+        if (bindingResult.hasErrors()) {
+            return "guests/signIn";
+        }
+        String username = userService.signIn(signInForm);
+        log.info("username : {}", username);
+        if (username == null) {
+            bindingResult.reject("signInFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "guests/signIn";
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.SIGN_IN_USER, username);
+        return "redirect:" + redirectURL;
     }
 }
