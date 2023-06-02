5dbc34fc29b90e0411e17b65dbf61c8e1c0d1d87
hossang <60059710+hossang@users.noreply.github.com>
2023-06-02 16:45:52 +0900

    feat(Item) : Add Item Domain

---
+++ src/main/java/hochang/ecommerce/domain/Item.java
@@ -0,0 +1,48 @@
+package hochang.ecommerce.domain;
+
+import hochang.ecommerce.util.file.UploadFile;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int count;
+
+    private long price;
+
+    @Lob
+    private String contents;
+
+    private String uploadFileName;
+
+    private String storeFileName;
+
+    @Builder
+    public Item(String name, int count, long price, String contents, String uploadFileName, String storeFileName) {
+        this.name = name;
+        this.count = count;
+        this.price = price;
+        this.contents = contents;
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+
+}
