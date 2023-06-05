4abb1f39091022e14019356c059f74deee3a6fce
hossang <60059710+hossang@users.noreply.github.com>
2023-06-05 18:13:21 +0900

    fix(ItemRegistrationForm) : Delete validation, add id

--- src/main/java/hochang/ecommerce/dto/ItemRegistrationForm.java
+++ src/main/java/hochang/ecommerce/dto/ItemRegistrationForm.java
@@ -4,21 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 @Getter
 @Setter
 public class ItemRegistrationForm {
-    @Size(max = 20, message = "제목은 20자 이내이어야 합니다")
+    private Long id;
+
     private String name;
 
     private int count;
 
-    private int price;
+    private long price;
 
     private String contents;
 
-    @Pattern(regexp = "^(image/jpeg|image/png)$", message = "JPG 또는 PNG 이미지만을 사용해야합니다.")
     private MultipartFile imageFile;
 }
