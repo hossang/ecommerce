6ac06920ec238ac82243f20ae124b22841dd0c7d
hossang <60059710+hossang@users.noreply.github.com>
2023-06-24 16:07:32 +0900

    feat(OrderRepository) : Add findByUserAndStatus() to search Order through username and OrderStatus

---
+++ src/main/java/hochang/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package hochang.ecommerce.repository;
+
+import hochang.ecommerce.domain.Order;
+import hochang.ecommerce.domain.OrderStatus;
+import hochang.ecommerce.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByUserAndStatus(User user, OrderStatus status);
+}
