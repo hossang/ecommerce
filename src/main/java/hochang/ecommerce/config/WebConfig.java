819ee743761ebccd2218283d811542be96d9c803
hossang <60059710+hossang@users.noreply.github.com>
2023-07-13 16:39:58 +0900

    feat(LogInterceptor) : Add interceptor to log prehandle(), postHandle, afterCompletion()

--- src/main/java/hochang/ecommerce/config/WebConfig.java
+++ src/main/java/hochang/ecommerce/config/WebConfig.java
@@ -2,6 +2,7 @@ package hochang.ecommerce.config;
 
 import hochang.ecommerce.web.annotation.SignInUserArgumentResolver;
 import hochang.ecommerce.web.interceptor.AuthInterceptor;
+import hochang.ecommerce.web.interceptor.LogInterceptor;
 import hochang.ecommerce.web.interceptor.SignInCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,12 +14,16 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SignInCheckInterceptor())
+        registry.addInterceptor(new LogInterceptor())
                 .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**");
+        registry.addInterceptor(new SignInCheckInterceptor())
+                .order(2)
                 .addPathPatterns("/users/**", "/admins/**")
                 .excludePathPatterns("/css/**", "/js/**");
         registry.addInterceptor(new AuthInterceptor())
-                .order(2)
+                .order(3)
                 .addPathPatterns("/users/**", "/admins/**")
                 .excludePathPatterns("/css/**", "/js/**");
 
