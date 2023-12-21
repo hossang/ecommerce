241f4c33b623c785f922aaa124f647d6635a774f
hossang <60059710+hossang@users.noreply.github.com>
2023-12-21 19:38:38 +0900

    feat(WebConfig) : Add ETag Header

--- src/main/java/hochang/ecommerce/config/WebConfig.java
+++ src/main/java/hochang/ecommerce/config/WebConfig.java
@@ -3,11 +3,17 @@ package hochang.ecommerce.config;
 import hochang.ecommerce.web.annotation.SignInUserArgumentResolver;
 import hochang.ecommerce.web.interceptor.LogInterceptor;
 import hochang.ecommerce.web.interceptor.SignInCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
+import java.time.Duration;
 import java.util.List;
 
 import static hochang.ecommerce.constants.NumberConstants.*;
@@ -19,12 +25,36 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LogInterceptor())
                 .order(INT_1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/css/**", "/js/**");
+                .excludePathPatterns("/css/**", "/js/**", "/*.ico", "/error");
         registry.addInterceptor(new SignInCheckInterceptor())
                 .order(INT_2)
-                .addPathPatterns("/users/**", "/admins/**")
+                .addPathPatterns("/users/**", "/admins/**", "/*.ico", "/error")
                 .excludePathPatterns("/css/**", "/js/**");
+
+        WebContentInterceptor webContentInterceptor = getCacheControlInterceptor();
+        registry.addInterceptor(webContentInterceptor)
+                .order(INT_3);
+
     }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>(new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/images/*");
+        filterRegistrationBean.setName("ETagFilter");
+        return filterRegistrationBean;
+    }
+
+    private WebContentInterceptor getCacheControlInterceptor() {
+        CacheControl cacheControl = CacheControl
+                .maxAge(Duration.ofSeconds(LONG_10))
+                .sMaxAge(Duration.ofSeconds(LONG_60));
+        WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+        webContentInterceptor.addCacheMapping(cacheControl, "/images/**");
+        return webContentInterceptor;
+    }
+
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
--- src/main/java/hochang/ecommerce/constants/NumberConstants.java
+++ src/main/java/hochang/ecommerce/constants/NumberConstants.java
@@ -4,6 +4,8 @@ public abstract class NumberConstants {
     public static final int INT_0 = 0;
     public static final int INT_1 = 1;
     public static final int INT_2 = 2;
+    public static final int INT_3 = 3;
+
     public static final int INT_5 = 5;
 
     public static final int INT_10 = 10;
@@ -15,5 +17,10 @@ public abstract class NumberConstants {
     public static final long LONG_0 = 0L;
     public static final long LONG_1 = 1L;
 
+    public static final long LONG_10 = 10L;
+
     public static final long LONG_30 = 30L;
+
+    public static final long LONG_60 = 60L;
+
 }
